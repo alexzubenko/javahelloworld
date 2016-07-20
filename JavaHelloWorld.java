479b02eaff2d6e1fcf40ee047d52b3aa8b68ae7b
alexzubenko <zubenkoalexandr@gmail.com>
2016-07-20 07:49:02 -0400

    Java second commit

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -3,5 +3,6 @@ public class JavaHelloWorld
                 {
                         System.out.println("Java Hello World");
                         System.out.println("Hello again");
+			System.out.println("Hello again 2222");
                 }
 }
